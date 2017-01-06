2297f8ae26409c45f2163da7bec79439faff51b8
RAINNI CHEN <rainni.chen@hotmail.com>
2017-01-06 00:44:20 -0500

    added comments to SC3D

--- Madelung_SC3D.m
+++ Madelung_SC3D.m
@@ -1,10 +1,28 @@
+%% 3D Simple Cubic Madelung Constant Calculator
+%
+% Author: Rainni Chen
+% Date: Oct 24 2016
+%
+% The madelung constant is defined as 
+% $\alpha\equiv\sum_j'\frac{(\pm)}{p_{ij}}$ where $j$ indexes all other
+% atoms and $p_{ij}$ is their separation divided by the lattice parameter.
+% This program calculates the madelung constant for a simple cubic
+% structure where the ith and (i+1)th set of nearest neighbors have equal
+% but opposite charges.
+%
+
+% select size to probe; volume of cube is (2*endpoint)^3
 endpoint = 100;
 
+% initialize the variable containing the madelung constant
 madelung = 0;
 
+% iterate through all atoms
 for i = -endpoint:endpoint
     for j = -endpoint:endpoint
         for k = -endpoint:endpoint
+            
+            % calculate the distance and its nearest neighbor index
             pij = sqrt(i^2+j^2+k^2);
             n = i+j+k;
             
@@ -14,6 +32,7 @@ for i = -endpoint:endpoint
                 n = 2;
             end
             
+            % add contribution from each atom
             if pij ~= 0
                 madelung = madelung + ((-1)^n/pij);
             end
@@ -21,4 +40,5 @@ for i = -endpoint:endpoint
     end
 end
 
+% display the result
 madelung
